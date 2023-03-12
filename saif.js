03c2a0d10124cdd8a7c44c94328c1d39c041f652
saifghanmi <102750526+saifghanmi@users.noreply.github.com>
2023-03-12 11:30:30 +0100

    Update saif.js

--- saif.js
+++ saif.js
@@ -1,24 +1,3 @@
-const cssFile = document.createElement('link');
-cssFile.rel = 'stylesheet';
-cssFile.href = 'file.css';
-document.head.appendChild(cssFile);
 
-
-
-
-  const urlParams = new URLSearchParams(window.location.search);
-  const darkmode54 = urlParams.get('darkmode54');
-
-  if (darkmode54 === '1') {
-    // enable your code here
-    
-    console.log("hello")
-
-    
-  } else {
-    // disable your code here
-    
     console.log("7obbi")
 
-    
-  }
